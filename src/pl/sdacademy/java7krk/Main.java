a357c4a87b205c4d76994bb033030b1027034dc4
Aneta <anetaryznar@gmail.com>
2017-11-07 22:44:06 +0100

    2

--- src/pl/sdacademy/java7krk/Main.java
+++ src/pl/sdacademy/java7krk/Main.java
@@ -1,8 +1,36 @@
 package pl.sdacademy.java7krk;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("podaj liczbe");
+        int liczba = scanner.nextInt();
+
+        for (int i = 0; i < liczba; i++) {
+            for (int j = 0; j < liczba; j++) {
+                if (i >= j) {
+                    System.out.print("#");
+                }
+            }
+
+            System.out.println();
+        }
+
+
+        for (int i = 0; i < liczba; i++) {
+            for (int j = 0; j < liczba - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
     }
 }
